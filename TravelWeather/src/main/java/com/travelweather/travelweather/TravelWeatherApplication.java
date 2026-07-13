7bac867bfbff9d1493c118211743fcc7b6b09780
Tadiya Brown <107151469+TadiyaBrown10@users.noreply.github.com>
2026-07-13 11:19:55 -0400

    Initial Spring Boot project setup

---
+++ TravelWeather/src/main/java/com/travelweather/travelweather/TravelWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.travelweather.travelweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelWeatherApplication.class, args);
+    }
+
+}
---
+++ TravelWeather/src/test/java/com/travelweather/travelweather/TravelWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelweather.travelweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
